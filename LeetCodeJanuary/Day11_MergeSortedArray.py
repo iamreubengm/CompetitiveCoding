759de816155b117df848c13b5b3006ddbd6fb7c6
Reuben George Mathew <mail4rgm@gmail.com>
2021-01-11 21:12:10 +0530

    Solution Added: Day 11

---
+++ LeetCodeJanuary/Day11_MergeSortedArray.py
@@ -0,0 +1,30 @@
+#Merge Sorted Array
+'''
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+The number of elements initialized in nums1 and nums2 are m and n respectively. 
+You may assume that nums1 has enough space (size that is equal to m + n)
+to hold additional elements from nums2.
+
+Example:
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    
+Solution:
+Compare the elements from the ends of each array.
+Take the larger element and put it into its corresponding place in nums1,
+and move the index variable accordingly.
+If there are leftover elements in nums2, add them to the beginning of nums1
+'''
+
+class Solution:
+    def merge(self,nums1,m,nums2,n):
+        while m and n:
+            if nums1[m-1]>=nums2[n-1]:
+                nums1[m+n-1]=nums1[m-1]
+                m-=1
+            else:
+                nums1[m+n-1]=nums2[n-1]
+                n-=1
+        if n:
+            nums1[:n]=nums2[:n]
