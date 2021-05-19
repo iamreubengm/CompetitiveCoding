fcb4407f1f1632a02fb8d13a77bdf38a818c864e
iamrgm <mail4rgm@gmail.com>
2021-05-19 22:12:26 +0530

    Solution Added: Day 19

---
+++ LeetCode/2021MonthlyChallenges/5_May/Day19_MinimumMovesToEqualArrayElementsII.py
@@ -0,0 +1,20 @@
+#Minimum Moves to Equal Array Elements II
+'''
+Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+In one move, you can increment or decrement an element of the array by 1.
+
+    Example:
+    Input: nums = [1,2,3]
+    Output: 2
+    Explanation: Only two moves are needed (remember each move increments or decrements one element):
+                 [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+'''
+
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        median=nums[len(nums)//2]
+        res=0
+        for i in nums:
+            res+=abs(i-median)
+        return res
