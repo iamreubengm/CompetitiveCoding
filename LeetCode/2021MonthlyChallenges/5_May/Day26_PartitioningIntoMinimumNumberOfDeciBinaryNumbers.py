593254213311db7f5a9c612613cca62fbb490684
iamrgm <mail4rgm@gmail.com>
2021-05-26 12:41:56 +0530

    Solution Added: Day 26

---
+++ LeetCode/2021MonthlyChallenges/5_May/Day26_PartitioningIntoMinimumNumberOfDeciBinaryNumbers.py
@@ -0,0 +1,16 @@
+#Partitioning Into Minimum Number Of Deci-Binary Numbers
+'''
+A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros.
+For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary
+numbers needed so that they sum up to n.
+
+Example:
+    Input: n = "32"
+    Output: 3
+    Explanation: 10 + 11 + 11 = 32
+'''
+
+class Solution:
+    def minPartitions(self,n):
+        return int(max(n))
