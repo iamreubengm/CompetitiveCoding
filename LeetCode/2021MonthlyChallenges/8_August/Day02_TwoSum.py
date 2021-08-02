69a42e2865a81f2eb3800cc7dc7fefc94fd0e67c
iamrgm <mail4rgm@gmail.com>
2021-08-02 20:02:12 +0530

    Solution Added: Day 2

---
+++ LeetCode/2021MonthlyChallenges/8_August/Day02_TwoSum.py
@@ -0,0 +1,19 @@
+#Two Sum
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
+
+class Solution:
+    def twoSum(self,nums,target):
+        d={}
+        for i,x in enumerate(nums):
+            if x in d:
+                return [d[x],i]
+            d[target-x]=i
