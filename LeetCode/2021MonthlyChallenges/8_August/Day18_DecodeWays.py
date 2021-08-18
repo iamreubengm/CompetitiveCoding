a1e792aafa4daa056323bbe67abb3d08ec2bb1ba
iamrgm <mail4rgm@gmail.com>
2021-08-18 21:32:23 +0530

    Solution Added: Day 18

---
+++ LeetCode/2021MonthlyChallenges/8_August/Day18_DecodeWays.py
@@ -0,0 +1,31 @@
+#Decode Ways
+'''
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+    'A' -> "1"
+    'B' -> "2"
+    'Z' -> "26"
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways).
+For example, "11106" can be mapped into:
+    "AAJF" with the grouping (1 1 10 6)
+    "KJF" with the grouping (11 10 6)
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+Given a string s containing only digits, return the number of ways to decode it.
+The answer is guaranteed to fit in a 32-bit integer.
+
+Example:
+    Input: s = "12"
+    Output: 2
+    Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+'''
+
+class Solution:
+    def numDecodings(self,s):
+        n=len(s)
+        d=[0]*(n+1)
+        d[n]=1
+        for i in range(n-1,-1,-1):
+            if s[i]!="0":
+                d[i]+=d[i+1]
+            if i<n-1 and (s[i]=="1" or s[i]=="2" and ord(s[i+1])<ord("7")):
+                d[i]+=d[i+2]
+        return d[0]
