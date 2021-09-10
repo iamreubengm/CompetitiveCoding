c792d7ea0b2de19e1e32d4ed35267590631702e0
iamrgm <mail4rgm@gmail.com>
2021-09-10 22:43:23 +0530

    Solution Added: Day 10

---
+++ LeetCode/2021MonthlyChallenges/9_September/Day10_ArithmeticSlicesIISubsequence.py
@@ -0,0 +1,35 @@
+#Arithmetic Slices II - Subsequence
+'''
+Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
+For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+The test cases are generated so that the answer fits in 32-bit integer.
+
+Example:
+    Input: nums = [2,4,6,8,10]
+    Output: 7
+    Explanation: All arithmetic subsequence slices are:
+        [2,4,6]
+        [4,6,8]
+        [6,8,10]
+        [2,4,6,8]
+        [4,6,8,10]
+        [2,4,6,8,10]
+        [2,6,10]
+'''
+
+from collections import Counter
+
+class Solution:
+    def numberOfArithmeticSlices(self,nums):
+        res=0
+        n=len(nums)
+        d=[Counter() for x in nums]
+        for i in range(n):
+            for j in range(i):
+                d[i][nums[i]-nums[j]]+=d[j][nums[i]-nums[j]]+1
+            res+=sum(d[i].values())
+        return res-(n-1)*n//2
