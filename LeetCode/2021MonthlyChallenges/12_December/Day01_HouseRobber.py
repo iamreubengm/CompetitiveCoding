2617f748b0053693e7f13e1ac7d38e4299635410
iamrgm <mail4rgm@gmail.com>
2021-12-01 11:08:03 +0530

    Solution Added: Day 1

---
+++ LeetCode/2021MonthlyChallenges/12_December/Day01_HouseRobber.py
@@ -0,0 +1,26 @@
+#House Robber
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
+the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example:
+    Input: nums = [1,2,3,1]
+    Output: 4
+    Explanation:
+        Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
+'''
+
+class Solution:
+    def rob(self,nums):
+        n=len(nums)
+        if n==1:
+            return nums[0]
+        dp=[0]*n
+        dp[0]=nums[0]
+        dp[1]=max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        return dp[-1]
