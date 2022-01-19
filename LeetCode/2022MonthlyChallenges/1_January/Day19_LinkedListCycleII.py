6fd03e544ebd23c64463d9ce855de0e34dcd2514
iamrgm <mail4rgm@gmail.com>
2022-01-19 23:53:41 +0530

    Solution Added: Day 19

---
+++ LeetCode/2022MonthlyChallenges/1_January/Day19_LinkedListCycleII.py
@@ -0,0 +1,33 @@
+#Linked List Cycle II
+'''
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+Do not modify the linked list.
+
+Example:
+    Input: head = [3,2,0,-4], pos = 1
+    Output: tail connects to node index 1
+    Explanation: There is a cycle in the linked list, where tail connects to the second node.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self,head):
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                break
+        else:
+            return None
+        while head!=slow:
+            head=head.next
+            slow=slow.next
+        return head
