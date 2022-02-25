87331adf7c11095b25ef3f32db0aeb2959b067a2
iamrgm <mail4rgm@gmail.com>
2022-02-25 22:17:15 +0530

    Solution Added: Day 25

---
+++ LeetCode/2022MonthlyChallenges/2_February/Day25_CompareVersionNumbers.py
@@ -0,0 +1,36 @@
+#Compare Version Numbers
+'''
+Given two version numbers, version1 and version2, compare them.
+Version numbers consist of one or more revisions joined by a dot '.'. Each revision consists of digits and may contain leading zeros.
+Every revision contains at least one character. Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, the next revision being revision 1, and so on.
+For example 2.5.33 and 0.1 are valid version numbers.
+
+To compare version numbers, compare their revisions in left-to-right order. Revisions are compared using their integer value ignoring any leading zeros.
+This means that revisions 1 and 001 are considered equal. If a version number does not specify a revision at an index, then treat the revision as 0.
+For example, version 1.0 is less than version 1.1 because their revision 0s are the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
+
+Return the following:
+    If version1 < version2, return -1.
+    If version1 > version2, return 1.
+    Otherwise, return 0.
+
+Example:
+    Input: version1 = "1.01", version2 = "1.001"
+    Output: 0
+    Explanation: Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+'''
+
+class Solution:
+    def compareVersion(self,version1,version2):
+        v1=[int(i) for i in version1.split('.')]
+        v2=[int(i) for i in version2.split('.')]
+        l1=len(v1)
+        l2=len(v2)
+        for i in range(max(l1,l2)):
+            t1=v1[i] if i<l1 else 0
+            t2=v2[i] if i<l2 else 0
+            if t1>t2:
+                return 1
+            elif t2>t1:
+                return -1
+        return 0
