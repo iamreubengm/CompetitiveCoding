15f8c7253e6405f08f995f503fa8b820ef3d1fa4
iamrgm <mail4rgm@gmail.com>
2022-03-12 22:13:23 +0530

    Solution Added: Day 12

---
+++ LeetCode/2022MonthlyChallenges/3_March/Day12_CopyListWithRandomPointer.py
@@ -0,0 +1,52 @@
+#Copy List with Random Pointer
+'''
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node.
+Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state.
+None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+    val: an integer representing Node.val
+    random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+Example:
+    Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+    Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+'''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self,head):
+        d=Node(-1)
+        d.next=head
+        cur=head
+        while cur:
+            tmp=Node(cur.val)
+            tmp.next=cur.next
+            cur.next=tmp
+            cur=tmp.next
+        cur=head
+        while cur:
+            if cur.random:
+                cur.next.random=cur.random.next
+            else:
+                cur.next.random=None
+            cur=cur.next.next
+        cur,nxt=d,head
+        while nxt:
+            cur.next=nxt.next
+            cur=nxt
+            nxt=cur.next
+        return d.next
